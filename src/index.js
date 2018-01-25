e9a74e7b8604071fe5c9426a97e9af3f80268c0f
kolaveridi <coderboysatya@gmail.com>
2018-01-25 22:19:23 +0530

    update

--- src/index.js
+++ src/index.js
@@ -11,7 +11,67 @@ class Main extends React.Component {
              gridfull:Array(this.rows).fill().map(() => Array(this.cols).fill(false))
          }
     }
-    
+     selectbox=(row,col)=>{
+         let gridcopy=arrayclone(this.state.gridfull);
+         gridcopy[row][col]=!gridcopy[row][col];
+
+         this.setState({
+             gridfull:gridcopy
+         })
+     }
+     seed=()=>{
+         console.log("seed");
+        let gridcopy=arrayclone(this.state.gridfull);
+        for(let i=0;i<this.rows;i++){
+            for(let j=0;j<this.cols;j++){
+                if(Math.floor(Math.random()*4)===1){
+                    console.log("random");
+                    gridcopy[i][j]=true;
+                }
+            }
+        }
+        this.setState({
+            gridfull:gridcopy
+        });
+       
+     }
+     playbutton =()=>{
+        clearInterval(this.intervalId);
+         this.intervalId=setInterval(this.play,this.speed);
+     }
+     play = () => {
+         console.log("play ");
+		let g = this.state.gridfull;
+		let g2 = arrayclone(this.state.gridfull);
+
+		for (let i = 0; i < this.rows; i++) {
+		  for (let j = 0; j < this.cols; j++) {
+            let count = 0;
+            console.log("count is"+count);
+		    if (i > 0) if (g[i - 1][j]) count++;
+		    if (i > 0 && j > 0) if (g[i - 1][j - 1]) count++;
+		    if (i > 0 && j < this.cols - 1) if (g[i - 1][j + 1]) count++;
+		    if (j < this.cols - 1) if (g[i][j + 1]) count++;
+		    if (j > 0) if (g[i][j - 1]) count++;
+		    if (i < this.rows - 1) if (g[i + 1][j]) count++;
+		    if (i < this.rows - 1 && j > 0) if (g[i + 1][j - 1]) count++;
+		    if (i < this.rows - 1 && this.cols - 1) if (g[i + 1][j + 1]) count++;
+            if (g[i][j] && (count < 2 || count > 3)) g2[i][j] = false;
+            
+		    if (!g[i][j] && count === 3) g2[i][j] = true;
+		  }
+		}
+		this.setState({
+		  gridFull: g2,
+		  generation: this.state.generation + 1
+		});
+
+	}
+
+     componentDidMount(){
+         this.seed();
+         this.playbutton();
+     }
     render(){
 
         return(
@@ -19,8 +79,8 @@ class Main extends React.Component {
 
            <h1> Game of life </h1>
 
-           
-           <Grid 
+
+           <Grid
            gridfull={this.state.gridfull}
            rows={this.rows}
            cols={this.cols}
@@ -71,19 +131,22 @@ class Box extends React.Component{
     selectbox =()=>{
         this.props.selectbox(this.props.row,this.props.col);
     }
+    
 render(){
 
     return(
-    <div>
+    <div
         className={this.props.boxclass}
         id={this.props.boxid}
         onClick={this.selectbox}
-        </div>
+      />
 
     );
 }
 
 }
-
+function arrayclone(arr) {
+        return JSON.parse(JSON.stringify(arr));
+}
 var approot=document.getElementById('appid');//CONNECT IT FIRST
 ReactDOM.render(<Main/>,approot);
